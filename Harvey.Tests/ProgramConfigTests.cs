80a00a5f5f6c4d0e5c86e4977d588bc2323d5037
dmlock <wibble99>
2015-01-25 10:59:28 +0000

    boilerplate program, logging, and config

---
+++ Harvey.Tests/ProgramConfigTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Harvey.Tests
+{
+    [TestClass]
+    public class ProgramConfigTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ProgramConfigCtorThrowsArgumentNullExceptionTest()
+        {
+            var p = new ProgramConfig(null);
+            Assert.IsNull(p);
+        }
+        [TestMethod]
+        public void ProgramConfigCtorSetsUpUrlParameterTest()
+        {
+            var p = new ProgramConfig(GetProgramArgsWithUrl());
+            Assert.AreEqual("Url", p.Url);
+        }
+        private string[] GetProgramArgsWithUrl()
+        {
+            var returnValue = new string[1];
+            returnValue[0] = "Url";
+            return (returnValue);
+        }
+    }
+}
---
+++ Harvey.Tests/ProgramTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Harvey.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ProgramCtorThrowsArgumentNullExceptionTest()
+        {
+            var logger = new Mock<ILogger>();
+            var p = new Program(logger.Object, null);
+            Assert.IsNull(p);
+        }
+        [TestMethod]
+        public void ProgramRunWithNoArgsLogsSomethingTest()
+        {
+            var logger = new Mock<ILogger>();
+            var config = new Mock<IProgramConfig>();
+            var args = GetProgramEmptyArgs();
+            var p = new Program(logger.Object, config.Object);
+            p.Start();
+            logger.Verify(c => c.Log(It.IsAny<string>(), It.IsAny<string[]>()), Times.Once);
+        }
+        private string[] GetProgramEmptyArgs()
+        {
+            var returnValue = new string[0];
+            return (returnValue);
+        }
+    }
+}
